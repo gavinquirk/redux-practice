72cf184703ae165ef6cb6dd4c1f046b99ec7ce2f
Gavin Quirk <gavinquirk@gmail.com>
2019-01-09 18:19:19 -0800

    delete result

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -31,6 +31,13 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({id: new Date(), value: state.counter}) // Concat allows for array manipulation immutably
       }
+    case 'DELETE_RESULT':
+      const updatedArray = state.results.filter(result => result.id !== action.resultElId) // Filter allows for array manipulation immutably
+      return {
+        ...state,
+        results: updatedArray
+
+      }
 
   }
   return state
