2263c02fb2aab6a56e33923d244b38c8c630d177
Gavin Quirk <gavinquirk@gmail.com>
2019-01-09 17:21:32 -0800

    increment, decrement, add, subtract

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,14 +29,31 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
 }
 
-export default Counter;
+// Map state to props --- pull in state from redux for use within component
+const mapStateToProps = state => { 
+    return {
+        ctr: state.counter
+    }
+}
+
+// Map dispatch to props --- pull in actions which will be executed from within component
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD'}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT'})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/store/reducer.js
@@ -0,0 +1,39 @@
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+
+  // INCREMENT
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  // DECREMENT
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  // ADD
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + 5
+    }
+  }
+
+  // SUBTRACT
+  if (action.type === 'SUBTRACT') {
+    return {
+      counter: state.counter - 5
+    }
+  }
+
+
+  return state
+}
+
+export default reducer
