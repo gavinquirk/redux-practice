09c75ad6df3ff1ae3315e7f32a2e0f435c08d85d
Gavin Quirk <gavinquirk@gmail.com>
2019-01-11 09:19:22 -0800

    outsource actions to separate file

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions'
+
 const initialState = {
   counter: 0,
   results: []
@@ -6,39 +8,38 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 
   switch (action.type) {
-    case 'INCREMENT':
+    case actionTypes.INCREMENT:
       return {
         ...state,
         counter: state.counter + 1
       }
-    case 'DECREMENT':
+    case actionTypes.DECREMENT:
       return {
         ...state,
         counter: state.counter - 1
       }
-    case 'ADD':
+    case actionTypes.ADD:
       return {
         ...state,
         counter: state.counter + action.val
       }
-    case 'SUBTRACT': 
+    case actionTypes.SUBTRACT: 
       return {
         ...state,
         counter: state.counter - action.val
       }
-    case 'STORE_RESULT':
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({id: new Date(), value: state.counter}) // Concat allows for array manipulation immutably
       }
-    case 'DELETE_RESULT':
+    case actionTypes.DELETE_RESULT:
       const updatedArray = state.results.filter(result => result.id !== action.resultElId) // Filter allows for array manipulation immutably
       return {
         ...state,
         results: updatedArray
 
       }
-
   }
   return state
 }
