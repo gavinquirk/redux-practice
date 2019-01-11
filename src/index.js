618ea8972ea5664f22e3e80d936a9cb47a27538e
Gavin Quirk <gavinquirk@gmail.com>
2019-01-11 14:13:29 -0800

    logger middleware, dev tools extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
@@ -15,8 +15,20 @@ const rootReducer = combineReducers({
   res: resultReducer
 })
 
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action)
+      const result = next(action)
+      console.log('[Middleware] next state', store.getState())
+      return result
+    }
+  }
+}
 
-const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 // Surround App with Provider component from react-redux. Set store prop to redux store
 
